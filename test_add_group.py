2039dc2c2b97be2510a986772a8dba21d407dd1b
Garanin Roman <garanin.r@gmail.com>
2018-06-18 23:20:08 +0300

    Commit lecture 2.2.

--- test_add_group.py
+++ test_add_group.py
@@ -1,68 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+def test_add_group(app):
+    # succes = True
 
-    def test_test_add_group(self):
-        succes = True
+    app.login(username = "admin", password = "secret")
+    app.create_group(Group(name="testgroup", header="testheader", footer="testfooter"))
+    app.logout()
+    # assertTrue(succes)
 
-        wd = self.wd
-
-        self.open_home_page(wd)
-        self.login(wd, username = "admin", password = "secret")
-        self.create_group(wd, Group(name="testgroup", header="testheader", footer="testfooter"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-        self.assertTrue(succes)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://192.168.0.41:8580")
-
-    def tearDown(self):
-        self.wd.quit()
-
-class test_simple_tests(unittest.TestCase):
-
-    def test_failed(self):
-        assert (1,2,3) == (1,2,3)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_failed(app):
+    assert (1,2,3) == (1,2,3)
