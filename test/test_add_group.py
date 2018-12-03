aac88f072940ddf0a51815cbde08bc365b64ed71
GaraninR <garanin.r@gmail.com>
2018-12-04 01:12:13 +0300

    Commit lecture 4_3

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,16 +8,24 @@ from model.group import Group
 def test_add_group(app):
     # succes = True
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="testgroup", header="testheader", footer="testfooter"))
+    group = Group(name="testgroup", header="testheader", footer="testfooter")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert_that(len(old_groups) + 1 == len(new_groups)).is_true()
     # assertTrue(succes)
+    old_groups.append(group)
+
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert_that(old_groups == new_groups)
 
 def test_passed_example(app):
     assert (1,2,3) == (1,2,3)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
+    old_groups.append(group)
     assert_that(len(old_groups) + 1 == len(new_groups)).is_true()
+    assert_that(old_groups == new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,9 +4,13 @@ from assertpy import *
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New name"))
+    group = Group(name="New name")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert_that(len(old_groups) == len(new_groups)).is_true()
+    old_groups[0] = group
+    assert_that(old_groups == new_groups)
 
 
 def test_modify_group_header(app):
